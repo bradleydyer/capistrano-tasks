92d81a06c0886847f665beed1dd65dfbbae64f4d
Anton McCook <asmccook@gmail.com>
2017-01-13 13:32:04 +0000

    Replaces domains with example urls

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,3 @@
-server 'web-04-eu.bd-ns.net', user: 'deploy', roles: %w{db web assets app}
-server 'web-05-eu.bd-ns.net', user: 'deploy', roles: %w{web}
-server 'web-06-eu.bd-ns.net', user: 'deploy', roles: %w{web}
-server 'web-07-eu.bd-ns.net', user: 'deploy', roles: %w{web}
+server 'web.example.com', user: 'deploy', roles: %w{db web}
 
 set :branch, ENV['branch'] || 'master'
